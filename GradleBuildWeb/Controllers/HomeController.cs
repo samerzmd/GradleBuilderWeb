f8b39f41e3dbf2bd517eec4d4b99003419279a5e
Sam.Z.D <samerzmd@gmail.com>
2016-04-22 19:37:21 +0300

    init

---
+++ GradleBuildWeb/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace GradleBuildWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var process = new Process
+            {
+                StartInfo =
+                            {
+                                FileName = "cmd.exe",
+                                WorkingDirectory = @"C:\Projects\TheApplication",
+                                UseShellExecute = false,
+                                RedirectStandardInput = true,
+                            },
+                EnableRaisingEvents = true
+
+            };
+            process.Exited += (sender, e) =>
+            {
+                Console.WriteLine("shiiiit");
+            };
+            process.Start();
+
+            process.StandardInput.WriteLine(@"gradlew.bat assembleDebug");
+            process.StandardInput.WriteLine("exit");
+
+            return View();
+        }
+    }
+}
---
+++ GradleBuildWeb/Models/BuildingProcess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GradleBuildWeb.Models
+{
+    
+    public class BuildingProcess: Process,IEquatable<BuildingProcess>
+    {
+
+        public BuildingProcess(string srcDirectory)
+        {
+            EnableRaisingEvents = true;
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                WorkingDirectory = srcDirectory,
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+            };
+        }
+        
+        public bool Equals(BuildingProcess other)
+        {
+            return StartInfo.WorkingDirectory.Equals(other.StartInfo.WorkingDirectory);
+        }
+    }
+}
